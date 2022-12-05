f072f1e0de6f6cff5ba533abddb1993c11485b80
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2022-12-05 05:57:36 +0200

    AddNewFieldsToRegistrationForm

---
+++ UserManagement/UserManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Models;
+
+namespace UserManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("Users", "Security");
+            builder.Entity<IdentityRole>().ToTable("Roles", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
+
+
+
+
+
+
+
+
+        }
+    }
+}
